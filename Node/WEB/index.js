1f0b2eba0be19db2ecb1437fe62f566c271b3126
Priscilla N <pnguyn@bu.edu>
2024-03-20 16:58:58 -0400

    modified Node/WEB/index.js

--- Node/WEB/index.js
+++ Node/WEB/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 app.use('/', express.static('frontend/build'));
 
 app.get('/api', (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello, world! This is secret #1');
 });
 
 app.listen(port, () => {
