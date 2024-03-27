26f04db2b2487acc1cec2cab693ee47c15ad1c85
Priscilla N <pnguyn@bu.edu>
2024-03-27 15:31:21 -0400

    secret2

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -2,6 +2,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
+    context.log('Secret2: '+ process.env['secret2']);
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
@@ -10,7 +11,11 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
+        
     };
+    
+    // Inserts a sample message into the queue
+    context.bindings.myQueueItem = "This is another test"
 
 };
 
